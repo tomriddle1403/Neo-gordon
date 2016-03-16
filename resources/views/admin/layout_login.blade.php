7b58619086e522381a39e52274857505824a4fbb
An Cao <kcj.pop.com@gmail.com>
2016-03-16 23:38:12 +0200

    Style login form a bit.

--- resources/views/admin/layout_login.blade.php
+++ resources/views/admin/layout_login.blade.php
@@ -1,27 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>@yield('title') &mdash; Gordon </title>
-	 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flat-ui/2.2.2/css/vendor/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <title>@yield('title') &mdash; Gordon </title>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flat-ui/2.2.2/css/vendor/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flat-ui/2.2.2/css/flat-ui.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-
 </head>
 <body>
-	<div class="container">
-		<div class="row vertical-center">
-			<div class="col-md-4"></div>
-			<div class="col-md-4">
-				<div class="panel panel-{{ $errors->any() ? 'danger' : 'default' }}">
-					<div class="panel-heading">
-						<h2 class="panel-title">@yield('heading')</h2>
-					</div>
-					<div class="panel-body">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <br>
+                <div class="panel panel-{{ $errors->any() ? 'danger' : 'default' }}">
+                    <div class="panel-heading">
+                        <h2 class="panel-title">@yield('heading')</h2>
+                    </div>
+                    <div class="panel-body">
                         @if($errors->any())
                             <div class="alert alert-danger">
                                 <strong>We found some errors</strong>
-
                                 <ul>
                                     @foreach($errors->all() as $error)
                                         <li>{{ $error }}</li>
@@ -30,13 +28,13 @@
                             </div>
                         @endif
 
-						@yield('content')
-					</div>
-				</div>
-			</div>
-			<div class="col-md-4"></div>
-		</div>
-	</div>
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
 <script src="//cdnjs.cloudflare.com/ajax/libs/flat-ui/2.2.2/js/vendor/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/flat-ui/2.2.2/js/flat-ui.min.js"></script>
 </body>
